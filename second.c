e8d3d00d712692be34fa056f07a7e8c8859e892d
orcasabitha <bsabithabe@gmail.com>
2014-08-20 15:06:07 +0530

    hi

---
+++ second.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a,b, sum;
+printf("enter the value of a:"\n);
+scanf("%d",&a);
+printf("enter the value of b:"\n);
+scanf("%d",&b);
+sum=a+b;
+printf("the sum of two numbers is %d",&sum);
+}

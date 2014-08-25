aa4d62789d7eddab46c085aaa2aeb14c6a046e9a
orcasabitha <bsabithabe@gmail.com>
2014-08-25 15:46:21 +0530

    CH3EXERCISE-4

---
+++ CH3EXER4.C
@@ -0,0 +1,17 @@
+/* chapter-3 Exercise-4
+	Sabitha 25,August 2014 */
+
+/* subtraction of given values */
+
+#include<stdio.h>
+
+int main(void)
+{
+
+	int a;
+	a = 87 - 15;
+	printf("The value of a is %d.\n", a);
+
+	return 0;
+
+}

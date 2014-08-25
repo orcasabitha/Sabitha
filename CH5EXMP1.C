99d07cc6a20be18f642143bbd6d511897350a83c
orcasabitha <bsabithabe@gmail.com>
2014-08-25 22:03:19 +0530

    ch5example1

---
+++ CH5EXMP1.C
@@ -0,0 +1,17 @@
+/* chapter-5 example-1
+	Sabitha 25,August 2014 */
+
+/* eighth triangular number */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int triangularnumber;
+
+	triangularnumber = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8;
+
+	printf("The eighth triangular number is %i\n", triangularnumber);
+
+	return 0;
+}

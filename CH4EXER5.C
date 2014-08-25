9d5fc5470060b88c0f2809702ea9d967508aa24a
orcasabitha <bsabithabe@gmail.com>
2014-08-25 22:02:02 +0530

    chapter4exercise5

---
+++ CH4EXER5.C
@@ -0,0 +1,18 @@
+/* chapter-4 exercise-5
+	Sabitha 25, August 2014 */
+
+/* output expected */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+	char d;
+
+	c = 'd';
+	d = c;
+	printf("d = %c\n", d);
+
+	return 0;
+}

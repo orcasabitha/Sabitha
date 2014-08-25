2ad69a8b428554c1a4e91644614538156b340cc0
orcasabitha <bsabithabe@gmail.com>
2014-08-25 15:46:38 +0530

    CH3EXERCISE5

---
+++ CH3EXER5.C
@@ -0,0 +1,19 @@
+/* chapter-3 exercise-5
+	Sabitha 25,August 2014 */
+
+/* finding error */
+
+#include<stdio.h>
+
+int main(void)
+{
+	int sum;
+
+	/* compute result */
+	sum = 25 + 37 - 19;
+
+	/* display results */
+	printf("The answer is %i.\n", sum);
+
+	return 0;
+}

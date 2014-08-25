20207994c13f4b608455760b5bfcd0d1735d7abe
orcasabitha <bsabithabe@gmail.com>
2014-08-25 15:47:01 +0530

    CH4EXAMPLE1

---
+++ CH4EXMP1.C
@@ -0,0 +1,29 @@
+/* chapter-4 example-1
+	Sabitha 25,August 2014 */
+
+/* basic datatypes */
+
+#include<stdio.h>
+
+
+int main(void)
+{
+
+	int a = 100;
+	float b = 331.79;
+	char c = 'S';
+	double d = 8.44e+11;
+
+
+	printf("a = %i\n", a);
+	printf("b = %f\n", b);
+	printf("c = %c\n", c);
+	printf("d = %e\n", d);
+	printf("d = %g\n", d);
+
+
+	return 0;
+
+}
+
+

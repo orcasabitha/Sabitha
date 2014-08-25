a6ef7b9ed4ab9e400934b3637a5ebe04f4327e05
orcasabitha <bsabithabe@gmail.com>
2014-08-25 22:02:36 +0530

    chapter4exercise7

---
+++ CH4EXER7.C
@@ -0,0 +1,16 @@
+/* chapter-4 exercise-7
+	Sabitha 25,August 2014 */
+
+/* exponential format expression */
+
+#include <stdio.h>
+
+int main(void)
+{
+	float a;
+	a = (3.31 * 10e-8 * 2.01 * 10e-7) / (7.16 * 10e-6 * 2.01 * 10e-8);
+
+	printf("a = %f.\n", a);
+
+	return 0;
+}

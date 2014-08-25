9a556a1c2f49abb9cc879ffc3c13689e49f2e8e2
orcasabitha <bsabithabe@gmail.com>
2014-08-25 15:45:59 +0530

    CH3EXERCISE-3

---
+++ CH3EXER3.C
@@ -0,0 +1,17 @@
+/* chapter-3 exercises-3
+	Sabitha 25,August 2014 */
+
+/* Expect output for following code */
+
+#include<stdio.h>
+
+int main(void)
+{
+
+	printf("Testing....");
+	printf(".....1");
+	printf(".....2");
+	printf("\n");
+
+	return 0;
+}

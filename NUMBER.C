7fc69f27c6741fb5c1d6a4828448d86a7ddd88cf
orcasabitha <bsabithabe@gmail.com>
2014-08-26 17:06:57 +0530

    mobilenumber

--- NUMBER.C
+++ NUMBER.C
@@ -8,15 +8,17 @@ int main(void)
 {
 	long long int number = 9444100000; 
 	long long int i;
+	long long int j;
 	
 	printf(" \n\n enter the values of i:\n");
     scanf("%lli", &i);
 
-	for( i=1; i<=99999; i++)
+	for( j=1; j<=i; j++)
 	{
 	number = number + 1;
 	printf("\n number =%lli\n", number);
 	}
 
+	
 	return 0;
 }

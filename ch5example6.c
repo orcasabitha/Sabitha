507e6c3d530b97e3eacb5af80b1a9c20f325d9cb
orcasabitha <bsabithabe@gmail.com>
2014-08-26 21:20:39 +0530

    ch5example6

---
+++ ch5example6.c
@@ -0,0 +1,18 @@
+/* chapter-5 example-6
+	Sabitha 26,August 2014 */
+	
+/* while statement */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int count = 1;
+	
+	while(count <= 5) {
+		printf(" %i \n", count);
+		++count;
+	}
+	
+	return 0;
+}

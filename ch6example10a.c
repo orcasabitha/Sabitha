af5271999ba011d600f83eb5b515b875097c227e
orcasabitha <bsabithabe@gmail.com>
2014-08-29 20:17:30 +0530

    ch6example10a

---
+++ ch6example10a.c
@@ -0,0 +1,28 @@
+/* chapter-6 example-10a
+	Sabitha 29,august 2014 */
+	
+/* Program to generate a table of prime numbers */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+	int p;
+	int d;
+	bool isPrime;
+	
+	for( p = 2; p <= 50; ++p ) {
+	isPrime = true;
+
+	for( d = 2; d < p; ++d )
+		if( p % d == 0 )
+			isPrime = false;
+		if( isPrime != false )
+			printf ("%i ", p);
+	}
+
+	printf("\n");
+
+	return 0;
+}

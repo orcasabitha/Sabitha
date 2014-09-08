1d69d61d97e6aabb3f427dc9936278a2283f1908
orcasabitha <bsabithabe@gmail.com>
2014-09-08 21:18:53 +0530

    ch8example4

---
+++ ch8example4.c
@@ -0,0 +1,24 @@
+/* chapter 8, example 4
+	Sabitha 6, sep 2014 */
+	
+/* Function to calculate the nth triangular number */
+
+#include <stdio.h>
+
+void calculateTriangularNumber(int n)
+{
+	int i, triangularNumber = 0;
+	
+	for( i = 1; i <= n; ++i )
+	triangularNumber += i;
+	printf("Triangular number %i is %i\n", n, triangularNumber);
+}
+
+int main(void)
+{
+	calculateTriangularNumber(10);
+	calculateTriangularNumber(20);
+	calculateTriangularNumber(50);
+
+	return 0;
+}

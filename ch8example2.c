8f7972c21908e640e723078a7ffdada5b3435423
orcasabitha <bsabithabe@gmail.com>
2014-09-08 09:18:36 +0530

    ch8example2

---
+++ ch8example2.c
@@ -0,0 +1,19 @@
+/* chapter 8, example 2
+	Sabitha 6, sep 2014 */
+	
+/* calling functions */
+	
+#include <stdio.h>
+
+void printMessage(void)
+{
+	printf ("Programming is fun.\n");
+}
+
+int main(void)
+{
+	printMessage();
+	printMessage();
+
+	return 0;
+}

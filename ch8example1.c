2d0015c4cfcfc0df9ee8ae7e6fcd2187e90be33c
orcasabitha <bsabithabe@gmail.com>
2014-09-08 09:18:11 +0530

    ch8example1

---
+++ ch8example1.c
@@ -0,0 +1,17 @@
+/* chapter 8, example 1
+	Sabitha 6, sep 2014 */
+	
+/* Writing a Function in C */
+
+#include <stdio.h>
+void printMessage(void)
+{
+	printf ("Programming is fun.\n");
+}
+
+int main(void)
+{
+	printMessage();
+
+	return 0;
+}

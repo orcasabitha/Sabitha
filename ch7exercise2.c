199f7ed79a6fe165294b193dadb116962efec153
orcasabitha <bsabithabe@gmail.com>
2014-09-08 09:14:36 +0530

    ch7exercise2

---
+++ ch7exercise2.c
@@ -0,0 +1,35 @@
+/* chapter 7, exercise 2
+	Sabitha 4, sep 2014 */
+	
+/* working with arrays, initializing array value to zero */
+
+#include <stdio.h>
+
+int main(void)
+{
+	float values[10];
+	int index;
+	float value;
+	float avg;
+	
+	for( index = 0; index < 10; ++index )
+	{
+	values[index] = 0;
+	printf("values[%i] = %f\n\n", index, values[index]); 
+	printf("enter the value\n");
+	scanf("%f", &values[index]);
+	printf("values[%i] = %f\n", index, values[index]);
+	}
+	
+	value = 0;
+	for( index = 0; index < 10; ++index )
+	{
+		value += values[index];
+		avg = value / 10;
+	}
+	printf("%f\n", avg);
+	
+	
+	return 0;
+}
+

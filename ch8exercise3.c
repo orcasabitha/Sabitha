e7a0bc0a362f59c3db740c142ceab2e8d69c7a92
orcasabitha <bsabithabe@gmail.com>
2014-09-13 21:44:22 +0530

    ch8exercise3

--- ch8exercise3.c
+++ ch8exercise3.c
@@ -1,34 +1,33 @@
 /* chapter 8 exercise 3
-	Sabitha 11, sep 2014 */
+	Sabitha 13,sep 2014 */
 	
-/* square root of a number with epsilon as an argument */
- 
- #include <stdio.h>
- 
- float absoluteval(float n)
- {
- 	if(n < 0)
- 		n = -n;
- 	
-	 return n;
- }
- 
- float sqrt1(float n,float epsilon)
- {
- 	float guess = 1.0;
- 	
- 	while(absoluteval(guess * guess - n) >= epsilon)
- 	
-		guess = ( n / guess + guess ) / 2.0;
- 
- 	return guess;
- }
- 
- int main(void)
- {
- 	printf("squreroot(4) = %f\n", sqrt1(4.0, 0.0001));
- 	printf("squreroot(144) = %f\n", sqrt1(149.7, 0.0002));
- 	
- 	return 0;
- }
- 
+/* Function to calculate the absolute value of a number */
+#include <stdio.h>
+
+float absoluteValue(float x)
+{
+	if( x < 0 )
+	x = -x;
+	
+	return(x);
+}
+
+/* Function to compute the square root of a number */
+
+float squareRoot(float x, float epsilon)
+{
+	float guess = 5;
+	while( absoluteValue (guess * guess - x) >= epsilon )
+	guess = ( x / guess + guess ) / 2.0;
+	
+	return guess;
+}
+
+int main(void)
+{
+	printf("squareRoot (2.0) = %f\n", squareRoot (2.0, 6));
+	printf("squareRoot (144.0) = %f\n", squareRoot (144.0, 0.007));
+	printf("squareRoot (17.5) = %f\n", squareRoot (17.5, 40));
+	
+	return 0;
+}

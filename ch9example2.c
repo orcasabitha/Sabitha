eff6170b693d8ae5aa3aa2110a1d971cc07719d5
orcasabitha <bsabithabe@gmail.com>
2014-09-20 17:39:51 +0530

    ch9example2

---
+++ ch9example2.c
@@ -0,0 +1,45 @@
+/* chapter 9 example 2
+	Sabitha 19, sep 2014 */
+	
+/* program to determine tommorows date */
+
+#include <stdio.h>
+
+int main (void)
+{
+	struct date
+	{
+		int month;
+		int day;
+		int year;
+	};
+	struct date today, tomorrow;
+	
+	const int dayspermonth[12] = { 31,28,31,30,31,30,31, 31, 30, 31, 30, 31};
+	
+	printf("enter todays date (mm dd yyyy): ");
+	scanf("%i %i %i", &today.month, &today.day, &today.year);
+	
+	if(today.day != dayspermonth[today.month - 1]) {
+		tomorrow.day =  today.day + 1;
+		tomorrow.month = today.month;
+		tomorrow.year = today.year;
+	}
+	
+	else if(today.month == 12) {
+		tomorrow.day = 1;
+		tomorrow.month = 1;
+		tomorrow.year = today.year + 1;
+	}
+	
+	else {
+		tomorrow.day = 1;
+		tomorrow.month = today.month + 1;
+		tomorrow.year = today.year;
+	}
+	
+	printf("tomorrows date is %i /%i /%.2i. \n", tomorrow.month, tomorrow.day, tomorrow.year % 100);
+	
+	return 0;
+}
+
